851a6a29bfbcda5eefbd04df983c1ec97e28bed4
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-04-18 16:07:21 -0300

    dependencies up to date

--- src/main/java/br/com/wagnersoft/babilonia/services/RemoteService.java
+++ src/main/java/br/com/wagnersoft/babilonia/services/RemoteService.java
@@ -1,21 +1,10 @@
 package br.com.wagnersoft.babilonia.services;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import javax.imageio.ImageIO;
-
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.pdfbox.Loader;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.ImageType;
-import org.apache.pdfbox.rendering.PDFRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +16,6 @@ import br.com.wagnersoft.babilonia.dominio.dto.WSResultDTO;
 import br.com.wagnersoft.babilonia.exceptions.BabiloniaException;
 import br.com.wagnersoft.babilonia.repository.CidadaoRepository;
 import br.com.wagnersoft.babilonia.utils.DateHelper;
-import jakarta.xml.bind.DatatypeConverter;
 
 /** Serviço de pesquisa de cidadão.
  * @author WagnerSoft
@@ -63,14 +51,6 @@ public class RemoteService implements BabiloniaService {
     WSResultDTO result = null;
     final LocalDate dtNasc = DateHelper.asLocalDate(cidadao.getNascimentoData());
     
-    // Busca do certificado
-    final Optional<CidDoc> docOpt = this.certRep.findDoc(cidadao.getRa());
-    String certData = docOpt.map(doc -> DateHelper.asFormatDate(doc.getId().getData(),"yyyyMMdd")).orElse(dtNasc.plusYears(18).getYear() + "0101");
-    String valData = docOpt.map(doc -> DateHelper.asFormatDate(doc.getValidadeData(),"yyyyMMdd")).orElse(dtNasc.plusYears(18).getYear() + "1231");
-
-    //Retornando NI quando nao tem nenhum
-    final DocEnum docEnum = docOpt.map(doc -> DocEnum.fromCodigo(doc.getTipo())).orElse(DocEnum.NI);
-
     // Montagem da resposta
     result = WSResultDTO.builder()
         .cpf(cidadao.getCpf())
@@ -87,57 +67,15 @@ public class RemoteService implements BabiloniaService {
         .consultaData(LocalDate.now())
         .build();
     
-    final SituacaoEnum situacao = this.situacaoHelper.verificaSituacao(cidadao);
-    String divergencias = this.situacaoHelper.avaliaDivergencias(result, consult);
-    result.setSituacaoCodigo(situacao.getCodigo());
-    result.setSituacaoDescricao(situacao.getDescricao() + divergencias);
-
     return result;
-  }
-  
-  /** Recupera a imagem do certificado.
-   * @param cpf
-   * @return
-   * @throws ConectagovException
-   */
-  public String consultDoc(final String cpf) throws BabiloniaException {
-    Optional<CidDoc> optional = Optional.empty();
-    if (!cpf.equals("")) {
-      optional = this.certRep.findDoc(cpf);
-    }
-    final CidDoc doc = optional.orElse(new CidDoc(id));
-    final DocEnum docEnum = DocEnum.fromCodigo(doc.getTipo());
-    if (docEnum == DocEnum.IDT || docEnum == DocEnum.NI) {
-      return DocEnum.fromCodigo(doc.getTipo()).getDescricao();
-    }
-    
-    if (doclog.getDocumento() == null || doclog.getDocumento().length == 0) {   
-      return "Não há certificado.";
-    }
-
-    try {
-      File file = new File("tempFile");
-      FileUtils.writeByteArrayToFile(file, doclog.getDocumento());
-      PDDocument docPdf = Loader.loadPDF(file);
-      final PDFRenderer pdfRenderer = new PDFRenderer(docPdf);
-      final BufferedImage bim = pdfRenderer.renderImageWithDPI(0, 300, ImageType.RGB);
-      try (final ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-        ImageIO.write(bim, "png", bos);
-        String imagem = "data:image/png;base64," + DatatypeConverter.printBase64Binary(bos.toByteArray());
-        LOGGER.debug("{}", imagem);
-        return imagem;
-      }
-    } catch (IOException e) {
-      throw new ConectagovException("Imagem não está disponível.", e);
-    }
   }
 
   public List<WSResultDTO> consultService(final String[] listaCpf) throws BabiloniaException {
     return this.consultService(listaCpf, 0);
   }
 
-  private Long testaRa(String ra) {
-    return StringUtils.isNotEmpty(ra) && ra.matches("-?\\d+(\\.\\d+)?") ? Long.valueOf(ra) : 0;
+  private Long testNumber(String param) {
+    return StringUtils.isNotEmpty(param) && ra.matches("-?\\d+(\\.\\d+)?") ? Long.valueOf(param) : 0;
   }
   
 } 
---
+++ src/main/java/br/com/wagnersoft/babilonia/utils/FileUtil.java
@@ -0,0 +1,37 @@
+package br.com.wagnersoft.babilonia.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import javax.imageio.ImageIO;
+
+import jakarta.xml.bind.DatatypeConverter;
+
+public class FileUtil {
+
+  private FileUtil() {
+  }
+  
+  public static String getImgString(final Byte[] byteArray) throws IOException{
+    File file = new File("tempFile");
+    FileUtils.writeByteArrayToFile(file, byteArray);
+    PDDocument docPdf = Loader.loadPDF(file);
+    final PDFRenderer pdfRenderer = new PDFRenderer(docPdf);
+    final BufferedImage bim = pdfRenderer.renderImageWithDPI(0, 300, ImageType.RGB);
+    try (final ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+      ImageIO.write(bim, "png", bos);
+      String imagem = "data:image/png;base64," + DatatypeConverter.printBase64Binary(bos.toByteArray());
+      LOGGER.debug("{}", imagem);
+      return imagem;
+    }
+  }
+
+}
