9ceb017483f88507cc039dfd43ddd831fb08aaaa
Wagner Lopes <wagner.luis.alopes@gmail.com>
2026-04-20 09:40:23 -0300

    test issue corrected again

--- src/test/java/br/com/wagnersoft/babilonia/apresentacao/v1/CidadaoControllerTest.java
+++ src/test/java/br/com/wagnersoft/babilonia/apresentacao/v1/CidadaoControllerTest.java
@@ -5,8 +5,10 @@ import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extension;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
