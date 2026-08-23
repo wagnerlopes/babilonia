a4f00cb55269cf3f74ba93d218b9a549cbe5f569
Wagner Lopes <wagnerlopes@gmail.com>
2026-08-23 12:07:05 -0300

    Aperfeiçoamento nos erros padrão da aplicação

--- src/main/java/br/com/wagnersoft/babilonia/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/wagnersoft/babilonia/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package br.com.wagnersoft.babilonia.exception;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -33,32 +31,36 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResultDTO> handleGeneralException(Exception e, HttpServletRequest request) {
-    LOGGER.debug("Erro interno na aplicação: ", e);
-    String msg = e.getCause().getMessage();
-    return buildErrorResponse(msg, TipoSituacao.ERRO_INTERNO, HttpStatus.INTERNAL_SERVER_ERROR);
-  }
-  
-  @ExceptionHandler(BabiloniaException.class)
-  public ResponseEntity<ErrorResultDTO> handleBabilonia(BabiloniaException e, HttpServletRequest request) {
-    LOGGER.debug("Requisição inválida: {}", e.getMessage());
-    String msg = e.getCause().getMessage();
-    return buildErrorResponse(msg, TipoSituacao.REQUISICAO_INVALIDA, HttpStatus.BAD_REQUEST);
+
+    String errorMessage = this.buildErrorMessage("Erro inesperado no servidor (HTTP 500)", e);
+
+    return buildErrorResponse(errorMessage, TipoSituacao.ERRO_INTERNO, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<ErrorResultDTO> handleValidationException(MethodArgumentNotValidException e, HttpServletRequest request) {
-    LOGGER.debug("Falha na validação de parâmetro: {}", e.getMessage());
-    List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
-    return buildErrorResponse(fieldErrors.toString() , TipoSituacao.ERRO_VALIDACAO, HttpStatus.BAD_REQUEST);
+  @ExceptionHandler(BabiloniaException.class)
+  public ResponseEntity<ErrorResultDTO> handleBabilonia(BabiloniaException e, HttpServletRequest request) {
+
+    String errorMessage = this.buildErrorMessage("Requisição inválida (HTTP 400)", e);
+
+    return buildErrorResponse(errorMessage, TipoSituacao.REQUISICAO_INVALIDA, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(NoDataFoundException.class)
   public ResponseEntity<ErrorResultDTO> handleNoDataFound(NoDataFoundException e, HttpServletRequest request) {
-    LOGGER.debug("Informação não encontrada: {}", e.getMessage());
-    String pathInfo = request.getPathInfo();
-    return buildErrorResponse(pathInfo, TipoSituacao.NAO_ENCONTRADO, HttpStatus.NOT_FOUND);
+
+    String errorMessage = this.buildErrorMessage("Informação não encontrada (HTTP 404)", e);
+
+    return buildErrorResponse(errorMessage, TipoSituacao.NAO_ENCONTRADO, HttpStatus.NOT_FOUND);
   }
-  
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResultDTO> handleValidationException(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+    String errorMessage = this.buildErrorMessage("Erro de validação de parâmetro (HTTP 406)", e);
+
+    return buildErrorResponse(errorMessage , TipoSituacao.ERRO_VALIDACAO, HttpStatus.NOT_ACCEPTABLE);
+  }
+
   /** Captura especificamente tokens que passaram da validade.
    * @param e @link ExpiredJwtException}
    * @param request @code HttpServletRequest}
@@ -66,9 +68,10 @@ public class GlobalExceptionHandler {
    */
   @ExceptionHandler(ExpiredJwtException.class)
   public ResponseEntity<ErrorResultDTO> handleExpiredJwt(ExpiredJwtException e, HttpServletRequest request) {
-    LOGGER.error("Token JWT expirado: {}", e.getMessage());
-    String msg = e.getMessage();
-    return buildErrorResponse(msg, TipoSituacao.TOKEN_EXPIRADO, HttpStatus.UNAUTHORIZED);
+
+    String errorMessage = this.buildErrorMessage("Token JWT expirado (HTTP 401)", e);
+
+    return buildErrorResponse(errorMessage, TipoSituacao.TOKEN_EXPIRADO, HttpStatus.UNAUTHORIZED);
   }
 
   /** Captura tokens adulterados, assinaturas erradas ou malformados.
@@ -78,12 +81,27 @@ public class GlobalExceptionHandler {
    */
   @ExceptionHandler({MalformedJwtException.class, SignatureException.class, IllegalArgumentException.class})
   public ResponseEntity<ErrorResultDTO> handleInvalidJwt(Exception e, HttpServletRequest request) {
-    LOGGER.error("Tentativa de acesso com Token JWT inválido: {}", e.getMessage());
-    String msg = e.getMessage();
-    return buildErrorResponse(msg, TipoSituacao.TOKEN_INVALIDO, HttpStatus.UNAUTHORIZED);
+
+    String errorMessage = this.buildErrorMessage("Tentativa de acesso com Token JWT inválido (HTTP 401): {}", e);
+
+    return buildErrorResponse(errorMessage, TipoSituacao.TOKEN_INVALIDO, HttpStatus.UNAUTHORIZED);
+  }
+
+  private String buildErrorMessage(String mensagem, Exception e) {
+
+    String mensagemErro = (e.getCause() != null && e.getCause().getMessage() != null) ? e.getCause().getMessage() : e.getMessage();
+
+    if (mensagemErro == null) {
+      mensagemErro = mensagem;
+    }
+
+    LOGGER.debug("{}", e);
+
+    return mensagemErro;
   }
 
   private ResponseEntity<ErrorResultDTO> buildErrorResponse(String descricao, TipoSituacao situacao, HttpStatus status) {
+
     ErrorResultDTO result = ErrorResultDTO.builder()
         .descricao(descricao)
         .consultaData(LocalDate.now())
